5149b6387775c49864a605ea418eff9b15124307
jonathanatger <jonathan.atger@yahoo.fr>
2020-09-13 15:56:25 +0200

    'contact message'

--- public/scripts/pages/Contact.js
+++ public/scripts/pages/Contact.js
@@ -8,7 +8,7 @@ export default class ContactSheet extends React.Component {
         <div className="contact-main">
             <div className='contact-welcome-message'>
                 <h2 className='bottom-spacing'> Don't be a stranger,</h2>
-                <h1>Say hello ! my dear</h1>
+                <h1>And say hello !</h1>
             </div>
             <div className='contact-links'>
                 <a target='_blank' href="mailto:jonathan.atger@yahoo.fr"><i class="fa fa-envelope"></i>  jonathan.atger@yahoo.fr</a>
