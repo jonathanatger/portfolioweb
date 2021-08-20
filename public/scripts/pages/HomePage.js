992e5b2bf4c2bd8b67df970d6e58488232237088
jonathanatger <jonathan.atger@yahoo.fr>
2021-08-20 12:05:53 +0200

    synch

--- public/scripts/pages/HomePage.js
+++ public/scripts/pages/HomePage.js
@@ -12,30 +12,30 @@ export default class HomePage extends React.Component {
                 <img src={Photo} className='hp-img'></img>   
             </div>  
             <div className='hp-description'>
-                <h2> Bonjour, je suis Jonathan Atger, un architecte et ingénieur spécialisé dans 
-                    les technologies utilisées dans la conception de bâtiments. 
-                </h2>
-                <h3>
-                Afin de mettre à profit mon expérience, j'enseigne (ESA/ESTP) et interviens auprès d'acteurs 
-                qui souhaitent s'engager davantage sur les outils numériques ; en particulier le BIM, la gestion de données et les outils de conception collaboratifs et paramétriques. 
-                Il m'est possible de :
+                <h3> Bonjour, je suis Jonathan Atger, un architecte et ingénieur <b>spécialisé dans 
+                    les technologies utilisées dans la conception de bâtiments</b>. 
                 </h3>
+                <h4>
+                Afin de mettre à profit mon expérience, <b>j'enseigne (ESA/ESTP) et interviens auprès d'acteurs 
+                qui souhaitent s'engager davantage sur les outils numériques</b> ; en particulier le BIM, la gestion de données et les outils de conception collaboratifs et paramétriques. 
+                Il m'est possible de :
+                </h4>
                 <ul>                    
-                    <h3>
-                        - Participer à définir la stratégie de mise en place d'outils numériques dans votre structure
-                    </h3>
-                    <h3>
-                        - Réaliser des missions de BIM Management et d'intervenir comme prestataire
-                    </h3>                
-                    <h3>
-                        - Dispenser des formations aux outils souhaités
-                    </h3>
+                    <h4>
+                        - Participer à définir la <b>stratégie de mise en place d'outils numériques</b> dans votre structure
+                    </h4>
+                    <h4>
+                        - Réaliser des missions de <b>BIM Management</b> et d'intervenir comme prestataire
+                    </h4>                
+                    <h4>
+                        - Dispenser des <b>formations</b> aux outils souhaités
+                    </h4>
                 </ul>
            
-                <h4> J'ai été impliqué dans l'adoption et la mise en oeuvre de technologies diverses au sein de cabinets d'architecture et bureaux d'études d'ingéniérie.
+                <h5> J'ai été impliqué dans l'adoption et la mise en oeuvre de technologies diverses au sein de cabinets d'architecture et bureaux d'études d'ingéniérie.
                     Parmi ces technologies, on compte le BIM mais aussi la réalité augmentée/virtuelle, la programmation visuelle et d'outils simples, les applications possibles du machine learning...
                     Ma double formation et des compétences de programmation me permettent d'avoir une vision large sur les possibilités qu'offrent le numérique dans nos pratiques.
-                </h4>
+                </h5>
             </div>  
             
         </div>
