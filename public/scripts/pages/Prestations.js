5ffb3bdb7a86aa196ba3747ce09a10d4179310bd
jonathanatger <jonathan.atger@yahoo.fr>
2021-08-31 15:57:34 +0200

    hyperlinks

--- public/scripts/pages/Prestations.js
+++ public/scripts/pages/Prestations.js
@@ -13,10 +13,10 @@ export default class Prestations extends React.Component {
     render(){
         return(
             <div>
-                <h1>Les missions que je propose</h1>
+                <h2 className='prestation-header'>Les prestations proposées sont regroupées dans quatre ensembles : <a href='#strategyimage'>stratégie de mise en place des outils numériques de conception,</a> <a href="#managerimage">BIM management,</a> <a href="#teachimage">formations aux outils numériques,</a> et <a href="#toolsimage">développement d'outils numériques personnalisés</a>.</h2>
                 <div className='prestation-row'> 
                         <div className='prestation-column'>
-                            <img src={strategyimage} className='prestation-img'></img>
+                            <img src={strategyimage} className='prestation-img' id='strategyimage'></img>
                             <h2 className='prestation-title'>Stratégie de mise en place d'outils numériques</h2>
                             <h3>Etat des lieux de l'adoption des outils numériques de conception liés au BIM et aux plateformes de gestion des données de projet dans votre structure</h3>
                             <h3>Aide au recrutement et au développement de fiches de poste </h3>
@@ -28,21 +28,21 @@ export default class Prestations extends React.Component {
                         <div className='prestation-divider'></div>
 
                         <div className='prestation-column'>
-                            <img src={managerimage} className='prestation-img'></img>
+                            <img src={managerimage} className='prestation-img' id='managerimage'></img>
                             <h2 className='prestation-title'>BIM Management</h2>
                         </div>
 
                         <div className='prestation-divider2'></div>
 
                         <div className='prestation-column'>
-                            <img src={teachimage} className='prestation-img'></img>
+                            <img src={teachimage} className='prestation-img' id='teachimage'></img>
                             <h2 className='prestation-title'>Formation</h2>
                         </div>
 
                         <div className='prestation-divider'></div>
 
                         <div className='prestation-column'>
-                            <img src={toolsimage} className='prestation-img'></img>
+                            <img src={toolsimage} className='prestation-img' id='toolsimage'></img>
                             <h2 className='prestation-title'>Développement d'outils personnalisés</h2>
                         </div>
                     </div>
