24845c917f0d4d4b7b199d7367d6562e59779161
jonathanatger <jonathan.atger@yahoo.fr>
2021-09-01 17:35:06 +0200

    rework prestations and homepage

--- public/scripts/pages/HomePage.js
+++ public/scripts/pages/HomePage.js
@@ -13,16 +13,12 @@ export default class HomePage extends React.Component {
             </div>  
             <div className='hp-description'>
                 <h3> Bonjour, je suis Jonathan Atger, un architecte et ingénieur <b>spécialisé dans 
-                    les technologies utilisées dans la conception de bâtiments.</b>     
+                    les technologies utilisées dans la conception de bâtiments.</b> Travaillant en indépendant dans la région parisienne, <b>j'interviens auprès d'acteurs  qui souhaitent s'engager davantage sur les outils numériques</b> ; en particulier le BIM, la gestion de données projet et les outils de conception collaboratifs et paramétriques. Pour votre structure, il m'est possible de :    
                 </h3>
-                <h4>
-                Afin de mettre à profit mon expérience, <b>j'enseigne (ESA/ESTP) et interviens auprès d'acteurs 
-                qui souhaitent s'engager davantage sur les outils numériques</b> ; en particulier le BIM, la gestion de données et les outils de conception collaboratifs et paramétriques. 
-                Il m'est possible de :
-                </h4>
+                
                 <ul>                    
                     <h4>
-                        - Participer à définir la <b>stratégie de mise en place d'outils numériques</b> dans votre structure
+                        - Participer à définir la <b>stratégie de mise en place d'outils numériques</b> au sein de votre organisation
                     </h4>
                     <h4>
                         - Réaliser des missions de <b>BIM Management</b> et d'intervenir comme prestataire
@@ -30,12 +26,17 @@ export default class HomePage extends React.Component {
                     <h4>
                         - Dispenser des <b>formations</b> aux outils souhaités
                     </h4>
+                    <h4>
+                        - <b>Développer des outils personnalisés</b> : site internet, plugins Revit et Dynamo
+                    </h4>
                 </ul>
-           
-                <h5> J'ai été impliqué dans l'adoption et la mise en oeuvre de technologies diverses au sein de cabinets d'architecture et bureaux d'études d'ingéniérie.
-                    Parmi ces technologies, on compte le BIM mais aussi la réalité augmentée/virtuelle, la programmation visuelle et d'outils simples, les applications possibles du machine learning...
-                    Ma double formation et des compétences de programmation me permettent d'avoir une vision large sur les possibilités qu'offrent le numérique dans nos pratiques.
-                </h5>
+
+                <h3 className='hp-link'><a href='/prestations'><i class="far fa-arrow-alt-circle-right"></i> Passer à la page des prestations </a></h3>                
+                
+                <h3><b>Pourquoi me contacter ?</b></h3>
+
+                <h3> J'ai été impliqué dans l'adoption et la mise en oeuvre de technologies diverses au sein de cabinets d'architecture et bureaux d'études d'ingéniérie. Parmi ces technologies, on compte le BIM mais aussi la réalité augmentée/virtuelle, la programmation visuelle et d'outils simples, les applications possibles du machine learning... Ma double formation et des compétences de programmation me permettent d'avoir une vision large sur les possibilités qu'offrent le numérique dans nos pratiques. J'enseigne la conception assistée par ordinateur (ESA/ESTP).
+                </h3>
             </div>  
             
         </div>
--- public/scripts/pages/Prestations.js
+++ public/scripts/pages/Prestations.js
@@ -1,10 +1,10 @@
 import React from 'react'
 import {Link} from 'react-router-dom'
 import '../../css/general.css' 
-import toolsimage from '../../content/tools.png' 
-import managerimage from '../../content/manager.png' 
-import strategyimage from '../../content/strategy.png' 
-import teachimage from '../../content/teach.png' 
+import toolsimage from '../../content/tools-color.png' 
+import managerimage from '../../content/manager-vcolor.png' 
+import strategyimage from '../../content/strategy-color.png' 
+import teachimage from '../../content/teach-color.png' 
 
 
 
@@ -13,47 +13,47 @@ export default class Prestations extends React.Component {
     render(){
         return(
             <div>
-                <h2 className='prestation-header'>Les prestations proposées sont regroupées dans quatre ensembles : <a href='#strategyimage'>stratégie de mise en place des outils numériques de conception,</a> <a href="#managerimage">BIM management,</a> <a href="#teachimage">formations aux outils numériques,</a> et <a href="#toolsimage">développement d'outils numériques personnalisés</a>.</h2>
+                <h4 className='prestation-header'>Les prestations proposées sont regroupées dans quatre ensembles : <a href='#strategyimage'>stratégie de mise en place des outils numériques de conception,</a> <a href="#managerimage">BIM management,</a> <a href="#teachimage">formations aux outils numériques,</a> et <a href="#toolsimage">développement d'outils numériques personnalisés</a>.</h4>
                 <div className='prestation-row'> 
                         <div className='prestation-column'>
                             <img src={strategyimage} className='prestation-img' id='strategyimage'></img>
-                            <h2 className='prestation-title'>Stratégie de mise en place d'outils numériques</h2>
-                            <h3>Etat des lieux de l'adoption des outils numériques de conception liés au BIM et aux plateformes de gestion des données de projet dans votre structure</h3>
-                            <h3>Aide au recrutement et au développement de fiches de poste </h3>
-                            <h3>Conseil au choix de solutions numériques </h3>
-                            <h3>Accompagnement sur projet pilote</h3>
-                            <h3>Support technique</h3>
-                            <h3>Développement des standards BIM de votre structure</h3>
+                            <h3 className='prestation-title'>Stratégie de mise en place d'outils numériques</h3>
+                            <h4>Etat des lieux de l'adoption des outils numériques de conception liés au BIM et aux plateformes de gestion des données de projet dans votre structure</h4>
+                            <h4>Aide au recrutement et au développement de fiches de poste </h4>
+                            <h4>Conseil au choix de solutions numériques </h4>
+                            <h4>Accompagnement sur projet pilote</h4>
+                            <h4>Support technique</h4>
+                            <h4>Développement des standards BIM de votre structure</h4>
                         </div>
 
                         <div className='prestation-divider'></div>
 
                         <div className='prestation-column'>
                             <img src={managerimage} className='prestation-img' id='managerimage'></img>
-                            <h2 className='prestation-title'>BIM Management</h2>
-                            <h3>Intervention en tant que BIM manager sur projet, ou en interne</h3>
-                            <h3>Rôle de BIM manager : développement d'objectifs BIM projet, audit d'expérience BIM des acteurs, tenue des réunions BIM, rédaction des protocoles, contrôle de modèles </h3>
-                            <h3>Rôle de coordinateur BIM interne : mise en conformité des modèles BIM livrés sur vos projets, créations d'objets dans les modèles selon besoin des projets, support technique</h3>                                                        
+                            <h3 className='prestation-title'>BIM Management</h3>
+                            <h4>Intervention en tant que BIM manager sur projet, ou en interne</h4>
+                            <h4>Rôle de BIM manager : développement d'objectifs BIM projet, audit d'expérience BIM des acteurs, tenue des réunions BIM, rédaction des protocoles, contrôle de modèles </h4>
+                            <h4>Rôle de coordinateur BIM interne : mise en conformité des modèles BIM livrés sur vos projets, créations d'objets dans les modèles selon besoin des projets, support technique</h4>                                                        
                         </div>
 
                         <div className='prestation-divider2'></div>
 
                         <div className='prestation-column'>
                             <img src={teachimage} className='prestation-img' id='teachimage'></img>
-                            <h2 className='prestation-title'>Formation</h2>
-                            <h3>Formations Revit et Dynamo</h3>
-                            <h3>Programmes personnalisés, profitant de mon expérience de l'enseignement et de formations réalisées auprès d'architectes et d'ingénieurs</h3>
-                            <h3>Sensibilisation aux enjeux du BIM et présentation de technologies émergentes dans la conception</h3>                            
+                            <h3 className='prestation-title'>Formations</h3>
+                            <h4>Formations Revit et Dynamo</h4>
+                            <h4>Programmes personnalisés, profitant de mon expérience de l'enseignement et de formations réalisées auprès d'architectes et d'ingénieurs</h4>
+                            <h4>Sensibilisation aux enjeux du BIM et présentation de technologies émergentes dans la conception</h4>                            
                         </div>
 
                         <div className='prestation-divider'></div>
 
                         <div className='prestation-column'>
                             <img src={toolsimage} className='prestation-img' id='toolsimage'></img>
-                            <h2 className='prestation-title'>Développement d'outils personnalisés</h2>
-                            <h3>Créations de sites Web pour le compte de votre structure</h3>
-                            <h3>Développement de plugins pour Revit dédiés à la productivité de vos équipes ou de fonctions personnalisées pour des projets spécifiques </h3>
-                            <h3>Développement de fonctions ou de blocs Dynamo </h3>                            
+                            <h3 className='prestation-title'>Développement d'outils personnalisés</h3>
+                            <h4>Créations de sites Web pour le compte de votre structure</h4>
+                            <h4>Développement de plugins pour Revit dédiés à la productivité de vos équipes ou de fonctions personnalisées pour des projets spécifiques </h4>
+                            <h4>Développement de fonctions ou de blocs Dynamo </h4>                            
                         </div>
                     </div>
                 
