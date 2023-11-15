f9ecd02e9f79b9d3829562dd5ef035533b07b48c
jonathanatger <jonathan.atger@yahoo.fr>
2023-11-15 21:45:15 +0100

    contact section work

--- public/scripts/pages/homepageSections/Contact.js
+++ public/scripts/pages/homepageSections/Contact.js
@@ -1,4 +1,8 @@
 import React from "react";
+import { Link } from "react-router-dom";
+import TwitterImage from "../../../content/ico-twitter-250.png";
+import WebImage from "../../../content/ico-web-100.png";
+import GithubImage from "../../../content/ico-github-96.png";
 
 const Curtain = function () {
   const pointRows = 10;
@@ -13,10 +17,10 @@ const Curtain = function () {
 
       points.push(
         <h3
-          key={i.toString()}
+          key={i.toString() + j.toString()}
           style={{
-            "grid-row": cssGridR,
-            "grid-column": cssGridC,
+            gridRow: cssGridR,
+            gridColumn: cssGridC,
           }}
         >
           .
@@ -28,20 +32,51 @@ const Curtain = function () {
   return <div className="contact-points">{points}</div>;
 };
 
+const ButtonMailto = ({ mailto, label }) => {
+  return (
+    <Link
+      to="#"
+      onClick={(e) => {
+        window.location.href = mailto;
+        e.preventDefault();
+      }}
+      className="contact-info-mail"
+    >
+      {label}
+    </Link>
+  );
+};
+const cvLinkText = "EN SAVOIR PLUS >";
+const mailLinkText = "ENVOYER UN MAIL >";
+
 const ContactInfo = function () {
   return (
     <div className="contact-info">
-      <div>
+      <div className="contact-info-intro">
         <h3 className="contact-info-text">
-          Je suis un coureur de fond de l'espace, avec un background en
-          architecture et ingéniérie, et une appétence pour le chocolat au lait.
-          Vous pouvez me contacter au numéro 3612 Papy Chulo
+          D'abord auto-didacte, j'ai appris à développer autour de l'écosystème
+          logiciel du monde de la construction. Par intérêt, j'ai continué dans
+          le data-science et le machine learning, le développement sous Unity,
+          et enfin sur le web avec une formation en ligne.
         </h3>
+        <h3 className="contact-info-text">
+          Précédemment chef de projet architecte et ingénieur .
+        </h3>
+        <Link to="/cv" className="contact-info-cv">
+          {cvLinkText}
+        </Link>
       </div>
-      <div>
-        <h1>Contact Info</h1>
-        <h1>Contact Info</h1>
-        <h1>Contact Info</h1>
+      <div className="contact-info-links">
+        <h2>Jonathan ATGER</h2>
+        <div className="contact-info-logos">
+          <img src={GithubImage} className="contact-image" alt="Github"></img>
+          <img src={TwitterImage} className="contact-image" alt="Twitter"></img>
+          <img src={WebImage} className="contact-image" alt="Webpage"></img>
+        </div>
+        <ButtonMailto
+          label="ENVOYER UN MAIL >"
+          mailto="mailto:jonathan.atger@gmail.com"
+        />
       </div>
     </div>
   );
