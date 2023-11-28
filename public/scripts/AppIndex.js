ed5b78a99fb364ff60b5b16368c7e69e48386727
jonathanatger <jonathan.atger@yahoo.fr>
2023-11-28 23:26:19 +0100

    reverting changes done to get useContext working

--- public/scripts/AppIndex.js
+++ public/scripts/AppIndex.js
@@ -1,32 +1,59 @@
 import { createRoot } from "react-dom/client";
-import React, { useState, useEffect } from "react";
+import React, { useState, useEffect, createContext, useContext } from "react";
 import HomePage from "./pages/HomePage.js";
 import NavigationBar from "./pages/NavigationBar.js";
 import _debounce from "lodash/debounce";
 import "../css/general.css";
 
+// to reference an object of the DOM we are creating :
+// const myBtn = useRef(null); initializes with null value
+// return (<button ref={myBtn}></button>); we reference the button likeso
+// const clickit = () => myBtn.current.click(); we can now use the reference in functions (don't forget 'current')
+// ------------------ to test useRef
+
+// To set a value from anywhere in the app
+// const lights = { happy: 0, sad: 1 };
+// const lightingMode = createContext(lights);
+// ------------- to test out
+
+let scrollPosition = 0;
+
+// const handleScrollPosition = () => {
+//   // debounceHandleScrollDirection(scrollPosition, window.scrollY);
+//   scrollPosition = window.scrollY;
+// };
+// window.addEventListener("scroll", handleScrollPosition);
+// const ScrollPositionContext = createContext(scrollPosition);
+
 const App = () => {
   const [scrollDirection, setScrollDirection] = useState("UP");
-  const [scrollPosition, setScrollPosition] = useState(0);
 
-  const handleScrollDirection = () => {
-    setScrollPosition((prevScrollPosition) => {
-      if (window.scrollY > prevScrollPosition) {
-        setScrollDirection("DOWN");
-      } else {
-        setScrollDirection("UP");
-      }
-      return window.scrollY;
-    });
+  //WIP ----------------------------------
+  // let scrollP = useContext(ScrollPositionContext);
+
+  // useEffect(() => {
+  //   console.log("useEffect triggered by scrollP");
+  // }, [scrollP]);
+
+  const handleScrollPosition = () => {
+    debounceHandleScrollDirection(scrollPosition, window.scrollY);
+    scrollPosition = window.scrollY;
   };
 
-  const debounceHandleScrollDirection = _debounce(handleScrollDirection, 50);
+  const handleScrollDirection = (prevScrollPosition, newScrollPosition) => {
+    if (newScrollPosition > prevScrollPosition && scrollDirection == "UP") {
+      setScrollDirection("DOWN");
+    } else {
+      setScrollDirection("UP");
+    }
+  };
+  const debounceHandleScrollDirection = _debounce(handleScrollDirection, 300);
 
   useEffect(() => {
-    window.addEventListener("scroll", debounceHandleScrollDirection);
+    window.addEventListener("scroll", handleScrollPosition);
 
     return () => {
-      window.removeEventListener("scroll", debounceHandleScrollDirection);
+      window.removeEventListener("scroll", handleScrollPosition);
     };
   }, []);
 
